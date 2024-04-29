e3d02aa7e8ab8349859d3c3b3dacb3c5fb787c7e
Tie Ma <tie.ma@anz.com>
2024-04-30 09:34:08 +1000

    edit file name

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { AppShell, Box, Burger, Group, Text, useMantineColorScheme } from '@mantine/core';
+import { ExcalidrawLocally, getExcalidrawLocally, saveExcalidrawLocally } from '@/models/storage';
 import { useEffect, useState } from 'react';
 
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
-
-import { useDisclosure } from '@mantine/hooks';
-import { ExcalidrawLocally, getExcalidrawLocally, saveExcalidrawLocally } from '@/models/storage';
-
+import { FileName } from '../FileName/FileName';
 import dynamic from 'next/dynamic';
+import { useDisclosure } from '@mantine/hooks';
+
 const Excalidraw = dynamic(async () => (await import('@excalidraw/excalidraw')).Excalidraw, {
   ssr: false,
 });
@@ -18,10 +18,19 @@ export function Welcome() {
   const { colorScheme } = useMantineColorScheme();
   const [data, setData] = useState<ExcalidrawLocally>();
   const [fileId, _setFileId] = useState<string>('untitled');
+  const [fileName, setFileName] = useState<string>('');
 
   useEffect(() => {
     setData(getExcalidrawLocally());
   }, []);
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+
+    setFileName(data.files[fileId].fileName);
+  }, [fileId]);
 
   if (!data) {
     return null;
@@ -42,7 +51,15 @@ export function Welcome() {
 
             <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
           </Group>
-          <Group>{data.files[fileId].fileName}</Group>
+          <Group>
+            <FileName
+              name={fileName}
+              onChange={(name) => {
+                data.files[fileId].fileName = name;
+                setFileName(name);
+              }}
+            />
+          </Group>
           <Group h="100%" px="md">
             <ColorSchemeToggle />
           </Group>
