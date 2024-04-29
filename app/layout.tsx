baee0568cf76b50028b4088709ce84604e184610
Tie (Marty) Ma <83256961+marty-anz@users.noreply.github.com>
2024-04-29 19:41:29 +1000

    change fav icon (#2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'Excalidraw locally',
+  description: 'Your excalidrawing never leaves your browser',
 };
 
 export default function RootLayout({ children }: { children: any }) {
